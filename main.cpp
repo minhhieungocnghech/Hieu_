1bfcea216328f5981fb8d9c29b437e5dcd46fabe
minhhieungocnghech <65522555+minhhieungocnghech@users.noreply.github.com>
2020-07-19 18:11:51 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "../include/Program.h"
+
+using namespace std;
+int main()
+{
+	Program program;
+	program.run();
+}
---
+++ program.h
@@ -0,0 +1,40 @@
+#ifndef _SHAPE_PROGRAM_H
+#define _SHAPE_PROGRAM_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "../include/Product.h"
+#include "../include/Conveyorbelt.h"
+#include "../include/Customer.h"
+
+
+using namespace std;
+
+#define ADD_PRODUCT 1
+#define ADD_COUNT   2
+#define SHOW_ALL    3
+#define EXIT        0
+
+class Program
+{
+private:
+    Conveyorbelt* conveyorbelt;
+    //vector<Product*> PName;
+public:
+    void run();
+    Program();
+    ~Program();
+private:
+
+    void print_menu() const;
+    int get_choice() const;
+    void do_task(const int &choice);
+    void add_product();
+    void add_count();
+    void show_all() const;
+    void print_message(const string &msg) const;
+};
+
+#endif
